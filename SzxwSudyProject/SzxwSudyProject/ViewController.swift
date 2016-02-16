6ad6c07c6780775edd7e5696fc168428d04cb2dd
wokuaidaojia <137758850@qq.com>
2016-02-16 02:21:12 -0500

    初始化项目
    先创建一个项目

---
+++ SzxwSudyProject/SzxwSudyProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SzxwSudyProject
+//
+//  Created by jack on 16/2/16.
+//  Copyright (c) 2016年 jack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
